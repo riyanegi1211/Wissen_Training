e18a97624ff38ae149be6868649e9aef932ac755
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2023-02-06 01:05:59 +0530

    SQL File

---
+++ Projects/Student Management/Project.sql
@@ -0,0 +1,6 @@
+//Create table Students
+create table STUDENTS(REG_NO number(3)primary key, NAME varchar(35), AGE number(2), STANDARD number(2), SCHOOL varchar(40));
+
+//Create table Subjects 
+create table SUBJECTS(REG_NO number(3), ENGLISH number(3),HINDI number(3),MATHS number(3),SCIENCE number(3),SOCIAL_SCIENCE number(3), 
+foreign key(REG_NO) references STUDENTS(reg_no));
