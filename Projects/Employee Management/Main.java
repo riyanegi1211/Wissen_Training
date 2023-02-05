f4868602d7e5d1ce2ec057015e3f367a27e50801
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2023-02-05 22:10:24 +0530

    Add files via upload

---
+++ Projects/Employee Management/Main.java
@@ -0,0 +1,12 @@
+import project.EmployeeManagement;
+import java.util.*;
+
+public class Main
+{
+   
+   public static void main(String Args[])
+   {
+        EmployeeManagement obj = new EmployeeManagement();
+		obj.run();
+   }
+}
