484e3f803582d02457a7017403c7a91cb8d8de46
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2023-02-05 22:05:44 +0530

    Delete java8prog.bat

--- Projects/Employee Management/java8prog.bat
+++
@@ -1,5 +0,0 @@
-set path=C:\Program Files\Java\jdk1.8.0_202\bin;.;%path%
-set classpath=.;%classpath%
-set path=C:\oraclexe\app\oracle\product\11.2.0\server\jdbc\lib;.;%path%
-set classpath=ojdbc6.jar;.;%classpath%
-start
