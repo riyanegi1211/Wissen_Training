32fbbdc403a3c0b3eb1131b75356c3a4646ce2f0
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2023-02-06 10:02:00 +0530

    Main File

---
+++ Projects/Student Management/MainMenu.java
@@ -0,0 +1,12 @@
+import project2.StudentManagement;
+import java.util.*;
+
+public class MainMenu
+{
+   
+   public static void main(String Args[])
+   {
+      StudentManagement obj = new StudentManagement();
+		obj.run();
+   }
+}
