c94f4bb5bb0fd879a5e80a4d9bc2ce180de9c698
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2023-03-05 16:38:50 +0530

    Add files via upload

---
+++ Maven/MavenProject/android/src/main/java/Wissen/App.java
@@ -0,0 +1,13 @@
+package Wissen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Mvn install copies the jar file into local repository" );
+    }
+}
