8f9f7c2f270bc98047d5d87267398d82e04bcc92
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2023-03-05 16:39:25 +0530

    Add files via upload

---
+++ Maven/java8prog.bat
@@ -0,0 +1,10 @@
+set path=C:\Program Files\Java\jdk1.8.0_202\bin;.;%path%
+set classpath=.;%classpath%
+set path=C:\oraclexe\app\oracle\product\11.2.0\server\jdbc\lib;.;%path%
+set classpath=ojdbc6.jar;.;%classpath%
+
+
+
+set path=C:\Program Files\Java\jdk1.8.0_202\bin;.;%path%;C:\Maven\apache-maven-3.9.0\bin
+
+start
